ed5864f9a2599d8aa4c43be5980613b7ddcb15ab
pocharlieoq <charlie05170@gmail.com>
2021-11-12 00:04:08 +0800

    test1

--- invoiceLottery/Form1.Designer.cs
+++ invoiceLottery/Form1.Designer.cs
@@ -28,20 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.addButton = new System.Windows.Forms.Button();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.invoiceListBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.yearComboBox = new System.Windows.Forms.ComboBox();
+            this.monthComboBox = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(309, 378);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 1;
+            this.addButton.Text = "新增";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(155, 379);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(148, 22);
+            this.inputTextBox.TabIndex = 2;
+            // 
+            // invoiceListBox
+            // 
+            this.invoiceListBox.FormattingEnabled = true;
+            this.invoiceListBox.ItemHeight = 12;
+            this.invoiceListBox.Location = new System.Drawing.Point(410, 61);
+            this.invoiceListBox.Name = "invoiceListBox";
+            this.invoiceListBox.Size = new System.Drawing.Size(310, 340);
+            this.invoiceListBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft JhengHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(12, 381);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "請輸入發票號碼：";
+            // 
+            // yearComboBox
+            // 
+            this.yearComboBox.FormattingEnabled = true;
+            this.yearComboBox.Location = new System.Drawing.Point(155, 339);
+            this.yearComboBox.Name = "yearComboBox";
+            this.yearComboBox.Size = new System.Drawing.Size(49, 20);
+            this.yearComboBox.TabIndex = 5;
+            this.yearComboBox.Text = "110";
+            // 
+            // monthComboBox
+            // 
+            this.monthComboBox.FormattingEnabled = true;
+            this.monthComboBox.Location = new System.Drawing.Point(232, 339);
+            this.monthComboBox.Name = "monthComboBox";
+            this.monthComboBox.Size = new System.Drawing.Size(59, 20);
+            this.monthComboBox.TabIndex = 6;
+            this.monthComboBox.Text = "07~08";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.monthComboBox);
+            this.Controls.Add(this.yearComboBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.invoiceListBox);
+            this.Controls.Add(this.inputTextBox);
+            this.Controls.Add(this.addButton);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "發票兌獎";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.ListBox invoiceListBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox yearComboBox;
+        private System.Windows.Forms.ComboBox monthComboBox;
     }
 }
 
--- invoiceLottery/Form1.cs
+++ invoiceLottery/Form1.cs
@@ -12,11 +12,28 @@ namespace invoiceLottery
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+            //comboBox加入可選擇的年份及月份
+            string[] years = { "109", "110" , "111"};
+            string[] months = { "01~02", "03~04", "05~06" ,"07~08","09~10","11~12"};
+            foreach (string year in years)
+            {
+                yearComboBox.Items.Add(year);
+            }
+            foreach (string month in months)
+            {
+                monthComboBox.Items.Add(month);
+            }
         }
 
-       
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            //將資料新增至list並顯示出來
+            invoiceListBox.Items.Add(yearComboBox.Text + "年" + monthComboBox.Text + "月\n發票號碼:" + inputTextBox.Text);
+            inputTextBox.Clear();
+        }
     }
 }
