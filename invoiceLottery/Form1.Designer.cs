1bbf3269b1b654f3c4ae87dfc603347e8fc3d770
pocharlieoq <charlie05170@gmail.com>
2021-11-12 15:20:26 +0800

    增加QRCode辨識功能

--- invoiceLottery/Form1.Designer.cs
+++ invoiceLottery/Form1.Designer.cs
@@ -28,12 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.addButton = new System.Windows.Forms.Button();
             this.inputTextBox = new System.Windows.Forms.TextBox();
             this.invoiceListBox = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
             this.yearComboBox = new System.Windows.Forms.ComboBox();
             this.monthComboBox = new System.Windows.Forms.ComboBox();
+            this.cameraPictureBox = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cameraComboBox = new System.Windows.Forms.ComboBox();
+            this.cameraStartButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.cameraPictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // addButton
@@ -90,11 +98,67 @@
             this.monthComboBox.TabIndex = 6;
             this.monthComboBox.Text = "07~08";
             // 
+            // cameraPictureBox
+            // 
+            this.cameraPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.cameraPictureBox.Location = new System.Drawing.Point(13, 62);
+            this.cameraPictureBox.Name = "cameraPictureBox";
+            this.cameraPictureBox.Size = new System.Drawing.Size(347, 264);
+            this.cameraPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.cameraPictureBox.TabIndex = 7;
+            this.cameraPictureBox.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 12);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "相機:";
+            // 
+            // cameraComboBox
+            // 
+            this.cameraComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cameraComboBox.Enabled = false;
+            this.cameraComboBox.FormattingEnabled = true;
+            this.cameraComboBox.Location = new System.Drawing.Point(68, 21);
+            this.cameraComboBox.Name = "cameraComboBox";
+            this.cameraComboBox.Size = new System.Drawing.Size(212, 20);
+            this.cameraComboBox.TabIndex = 9;
+            // 
+            // cameraStartButton
+            // 
+            this.cameraStartButton.Enabled = false;
+            this.cameraStartButton.Location = new System.Drawing.Point(294, 20);
+            this.cameraStartButton.Name = "cameraStartButton";
+            this.cameraStartButton.Size = new System.Drawing.Size(75, 23);
+            this.cameraStartButton.TabIndex = 10;
+            this.cameraStartButton.Text = "開始偵測";
+            this.cameraStartButton.UseVisualStyleBackColor = true;
+            this.cameraStartButton.Click += new System.EventHandler(this.cameraStartButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(410, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(310, 22);
+            this.textBox1.TabIndex = 11;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.cameraStartButton);
+            this.Controls.Add(this.cameraComboBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cameraPictureBox);
             this.Controls.Add(this.monthComboBox);
             this.Controls.Add(this.yearComboBox);
             this.Controls.Add(this.label1);
@@ -103,6 +167,9 @@
             this.Controls.Add(this.addButton);
             this.Name = "Form1";
             this.Text = "發票兌獎";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.cameraPictureBox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -115,6 +182,12 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox yearComboBox;
         private System.Windows.Forms.ComboBox monthComboBox;
+        private System.Windows.Forms.PictureBox cameraPictureBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cameraComboBox;
+        private System.Windows.Forms.Button cameraStartButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- invoiceLottery/Form1.cs
+++ invoiceLottery/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using ZXing;
 
 namespace invoiceLottery
 {
@@ -16,24 +19,95 @@ namespace invoiceLottery
         public Form1()
         {
             InitializeComponent();
-            //comboBox加入可選擇的年份及月份
-            string[] years = { "109", "110" , "111"};
-            string[] months = { "01~02", "03~04", "05~06" ,"07~08","09~10","11~12"};
-            foreach (string year in years)
-            {
-                yearComboBox.Items.Add(year);
-            }
-            foreach (string month in months)
-            {
-                monthComboBox.Items.Add(month);
-            }
         }
 
+        FilterInfoCollection filterInfoCollection;
+        VideoCaptureDevice videoCaptureDevice;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //comboBox加入可選擇的年份及月份
+            string[] years = { "109", "110", "111" };
+            string[] months = { "01~02", "03~04", "05~06", "07~08", "09~10", "11~12" };
+            foreach (string year in years)
+                yearComboBox.Items.Add(year);
+            foreach (string month in months)
+                monthComboBox.Items.Add(month);
+            //讀取可用的webcam
+            filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            if (filterInfoCollection != null)
+            {
+                foreach (FilterInfo filter in filterInfoCollection)
+                    cameraComboBox.Items.Add(filter.Name);
+                cameraComboBox.SelectedIndex = 0;
+                cameraComboBox.Enabled = true;
+                cameraStartButton.Enabled = true;
+            }
+            else
+            {
+                cameraComboBox.Text = "無視訊鏡頭";
+            }
+        }
         private void addButton_Click(object sender, EventArgs e)
         {
             //將資料新增至list並顯示出來
             invoiceListBox.Items.Add(yearComboBox.Text + "年" + monthComboBox.Text + "月\n發票號碼:" + inputTextBox.Text);
             inputTextBox.Clear();
+        }
+
+        private void cameraStartButton_Click(object sender, EventArgs e)
+        {
+            //開始或結束偵測按鈕
+            if (videoCaptureDevice == null)
+            {
+                videoCaptureDevice = new VideoCaptureDevice(filterInfoCollection[cameraComboBox.SelectedIndex].MonikerString);
+                videoCaptureDevice.NewFrame += VideoCaptureDevice_NewFrame;
+                videoCaptureDevice.Start();
+                timer1.Start();
+                cameraStartButton.Text = "結束偵測";
+            }
+            else
+            {
+                    videoCaptureDevice.Stop();
+                    videoCaptureDevice=null;
+                    timer1.Stop();
+                    cameraStartButton.Text = "開始偵測";
+            }
+
+        }
+
+        private void VideoCaptureDevice_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+           //更新畫面
+            cameraPictureBox.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //離開時把相機跟timer關掉
+            if (videoCaptureDevice != null)
+            {
+                videoCaptureDevice.Stop();
+                videoCaptureDevice = null;
+                timer1.Stop();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //偵測QRCode
+            if (cameraPictureBox.Image != null)
+            {
+                BarcodeReader reader = new BarcodeReader();
+                Result result = reader.Decode((Bitmap)cameraPictureBox.Image);
+                if (result != null && result.ToString() != textBox1.Text)
+                {
+                    //把結果更新到textbox上
+                    textBox1.Text = result.ToString();
+                    // if (videoCaptureDevice.IsRunning)
+                    //videoCaptureDevice.Stop();
+                    //timer1.Stop();
+                }
+            }
         }
     }
 }
